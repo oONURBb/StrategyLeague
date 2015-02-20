92d2a91aedc43e9c834aa7e37244d76f9dc5ce2e
Daniel <danivangelis@hotmail.com>
2015-02-20 21:54:27 +0000

    Attributes update automatically now

--- index.php
+++ index.php
@@ -9,9 +9,40 @@
 	?>
 	<title>StrategyLeague</title>
 	<link href="mystyle.css" rel="stylesheet" />
+	<script src="Scripts/jquery-1.11.2.js"></script>
 </head>
+<script>
 
-<body>
+	function updateAtr() {
+		var id = <?=$_SESSION["user_id"] ?>;
+		var response = "";
+		$.ajax({
+			type: "GET",
+			url: "getAttr.php?id=" + id,
+			dataType : "json",
+			async: false,
+			success : function(text){
+				 response = text;
+			}
+			
+		});
+		$("#ad").text(response['ad']);
+		$("#ap").text(response['ap']);
+		$("#speed").text(response['speed']);
+		$("#armor").text(response['armor']);
+		$("#mr").text(response['mr']);
+	}
+
+	$(document).ready(
+		function() {
+			setInterval(function(){
+				updateAtr();
+			}, 5000);
+		}
+	)
+	
+</script>
+<body onload="updateAtr()">
 	
 	<div id="charInfo">
 		<div id="avatar">
@@ -22,7 +53,11 @@
 	</div>
 	
 	<div id="atr">
-		
+		<div class="atr"><b>AD:</b> <div id="ad" class="atr"></div></div>
+		<div class="atr"><b>AP:</b> <div id="ap" class="atr"></div></div>
+		<div class="atr"><b>SPEED:</b> <div id="speed" class="atr"></div></div>
+		<div class="atr"><b>ARMOR:</b> <div id="armor" class="atr"></div></div>
+		<div class="atr"><b>MR:</b> <div id="mr" class="atr"></div></div>
 	</div>
 	
 	<div id="game"></div>
